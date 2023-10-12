ffc57c71ac4ed44ff41fc14e9e2747607c09bd82
JitukawaHirotaka <jitu8726@gmail.com>
2023-10-12 11:42:33 +0900

    feature: add install setting

--- install.sh
+++ install.sh
@@ -1,3 +1,12 @@
 #!/usr/bin/env bash
 
 set -ue
+
+brew bundle install
+
+# symbolic link
+ln -sf ${$(dirname $0)}/.vimrc ~/
+ln -sf ${$(dirname $0)}/.config/zsh/.zshrc ~/
+
+# iTerm2
+cp -f iTerm2/com.googlecode.iterm2.plist ~/Library/Preferences
