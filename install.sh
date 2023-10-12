8dadbaf616dbb678897ac73f28cbf0f6605504f5
JitukawaHirotaka <jitu8726@gmail.com>
2023-10-12 15:12:19 +0900

    feature: install が実行できるように変更

--- install.sh
+++ install.sh
@@ -2,11 +2,16 @@
 
 set -ue
 
-brew bundle install
+cd `dirname $0`
 
 # symbolic link
-ln -sf ${$(dirname $0)}/.vimrc ~/
-ln -sf ${$(dirname $0)}/.config/zsh/.zshrc ~/
+ln -sf "$(pwd)"/.vimrc ~/
+ln -sf "$(pwd)"/.config/zsh/.zshrc ~/
+ln -sf "$(pwd)"/.config/zsh/rc/alias.zsh ~/
+
+# apply
+source ~/.vimrc
+source ~/.zshrc
 
 # iTerm2
 cp -f iTerm2/com.googlecode.iterm2.plist ~/Library/Preferences
