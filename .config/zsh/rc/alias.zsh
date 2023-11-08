153d904e0038be13202a967b8ae1dfda10b88292
JitukawaHirotaka <jitu8726@gmail.com>
2023-11-08 12:23:46 +0900

    feature: chrome表示のaliasを追加

--- .config/zsh/rc/alias.zsh
+++ .config/zsh/rc/alias.zsh
@@ -1,3 +1,6 @@
 # common
 alias c='clear'
 alias ..='cd ..'
+
+# chromeをローカルファイル表示用に立ち上げる
+alias ochrome='open /Applications/Google\ Chrome.app/ --args --disable-web-security --user-data-dir --incognito --allow-file-access-from-files'
